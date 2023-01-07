beb9ec39d26a2f6f4557266483ce24e1dc4e4a73
yycsec <github@devsec.one>
2023-01-06 23:20:48 -0700

    Updating solution for problem 3.

--- src/3.py
+++ src/3.py
@@ -6,6 +6,5 @@ result = 0
 while num > 1:
     factor = utils.find_first_factor(num)
     num //= factor
-    if utils.is_prime(factor) and factor > result:
-        result = factor
+    result = max(result, factor)
 print(result)
